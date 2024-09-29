b197d67a2fa6ecd2ea4f02b79cbbb98d0e7f17bd
Anees ur rehman <aneesurrehman075@gmail.com>
2024-09-29 13:42:34 -0700

    30-09-202

---
+++ OOP/constrictor_destructor.py
@@ -0,0 +1,19 @@
+class ClassSchedule:
+   def __init__(self,name,age,gmail):
+       self.__name = name # private
+       self._age = age # protected
+       self.gmail = gmail # public 
+       print("constructor called ")
+  
+   def __del__(self):
+       print('Destructor called')
+   def display(self):
+       print(f"{self.__name},{self._age},{self.gmail}")
+       
+
+obj = ClassSchedule('anees','12','anees@gmail.com')
+
+obj.display()
+obj.gmail ="ikram@gmail.com" # can accees publically
+
+obj.display()
